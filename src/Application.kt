8375b77a8802b0c6e32dc119f4b079f07b5ef8fa
Reza Maulana <rflash95@gmail.com>
2020-12-31 15:12:18 +0700

    initial commit

---
+++ src/Application.kt
@@ -0,0 +1,35 @@
+package com.learning
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+
+        var weather ="sunny"
+        get("/weatherforecast"){
+            call.respondText(weather, contentType = ContentType.Text.Plain)
+        }
+
+        post("weatherforecast"){
+            weather = call.receiveText()
+            call.respondText(
+                "The weather has been set to : $weather",
+                contentType = ContentType.Text.Plain
+            )
+
+        }
+    }
+}
+
